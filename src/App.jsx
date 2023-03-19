0ff420db34da43f6420875b3ed7412172a70a983
Divine Edwin <edwindivine5@gmail.com>
2023-03-19 13:36:43 +0100

    Styling of cat list.
    Used sass for styling the cat-lists.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
 import axios from 'axios'
 import { useQuery } from 'react-query'
+import './styles/style.scss'
 
 const App = () => {
   // const [count, setCount] = useState(0)
@@ -13,19 +13,21 @@ console.log(data)
   if(error) return <div>Error: {error.message}</div>
 
   return (
-    <div>
-      {/* <img src={data[2].url} alt="cat-img" height={data.height} width={data.width}/> */}
+    <>
+      <h1 className='title'>Cat Images and profile in the World</h1>
+    <div className='cat-grid'>
       { data.map(cat => (
-        <div key={cat.id}>
-          <img src={cat.url} alt="cat-img" />
-          <p>Name: {cat.breeds[0].name}</p>
-          <p>Description: {cat.breeds[0].description}</p>
+        <div className='cat' key={cat.id}>
+          <img className='cat-img' src={cat.url} alt="cat-img" />
+          <p className='cat-name'>{`Name: ${cat.breeds[0].name}`}</p>
+          {/* <p>Description: {cat.breeds[0].description}</p>
           <p>Temperament: {cat.breeds[0].temperament}</p>
           <p>Imperial:{cat.breeds[0].weight.imperial} - Metric:{cat.breeds[0].weight.metric}</p>
-          <p>{cat.breeds[0].wikipedia_url}</p>
+          <p>{cat.breeds[0].wikipedia_url}</p> */}
         </div>
       ))}
     </div>
+    </>
   )
 }
 
